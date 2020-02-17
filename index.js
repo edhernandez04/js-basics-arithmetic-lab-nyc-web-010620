8190459b0402b1b37993421f0bdb212f5bfb106f
edhernandez04 <mchh.004@gmail.com>
2020-02-17 13:06:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here
+let var1 = 31
+let var2 = 2
+
+const multiply = var1 * var2
+
+const random = Math.random() + 1
+
+const mod = 103 % 9
+
+const arr = [1,20]
+
+const max = Math.max(...arr)
